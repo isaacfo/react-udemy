02d7521b5d1016c354332ad4d5efc43b423bb227
Isaac Fonseca <isaacfo1010@gmail.com>
2018-12-21 11:30:32 -0500

    added Radium for enabeling hover and media query effects

--- src/App.js
+++ src/App.js
@@ -1,61 +1,142 @@
 import React, { Component } from 'react'; 
 import './App.css';
 import Person from './Person/Person'; 
+// have to import styleroot so tranforming i.e. media quieries work
+import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
   state = {
     persons: [
-      {name: 'Max', age: 28},
-      {name: 'Manu', age: 29},
-      {name: 'Stephanie', age: 26}
+      {id: 'jo', name: 'Max', age: 28},
+      {id: 'fd', name: 'Manu', age: 29},
+      {id: 'sad', name: 'Stephanie', age: 26}
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
+// Method
+//   switchNameHandler = (newName) => {
+//     // console.log('Was clicked');
+//     // DON'T DO THIS: React doesn't recognize a direct change like this example; this.state.person[0] = 'Maximillian'
+//     this.setState({
+//     persons: [
+//       {name: newName, age: 28},
+//       {name: 'Manu', age: 29},
+//       {name: 'Stephanie', age: 27}
+//     ]
+//   })
+// }
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked');
-    // DON'T DO THIS: React doesn't recognize a direct change like this example; this.state.person[0] = 'Maximillian'
-    this.setState({
-    persons: [
-      {name: newName, age: 28},
-      {name: 'Manu', age: 29},
-      {name: 'Stephanie', age: 27}
-    ]
-  })
-}
+// Method
+nameChangedHandler = (event, id) => {
+  const personIndex = this.state.persons.findIndex(p => {
+    return p.id === id;
+  });
+
+  const person = {
+    ...this.state.persons[personIndex]
+  };
+
+  person.name = event.target.value;
+
+  const persons = [...this.state.persons];
+  persons[personIndex]= person;
+
+  
 
 
-nameChangedHandler = (event) => {
-  this.setState( {
-    persons: [
-      {name: 'Max', age: 28},
-      {name: event.target.value, age: 29},
-      {name: 'Stephanie', age: 26}
-    ]
-  })
+  this.setState( {persons: persons})
+
 } 
 
+// Method
+togglePersonsHandler= () => {
+  const doesShow = this.state.showPersons;
+  this.setState({showPersons:!doesShow});
+
+}
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+
+
+  }
+
+
   render() {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      // its not a valid js properties name cuz of colon, as strings they are valid. css sudo selector enabled by Radium
+      ':hover': {
+        backgroundColor:'lightgreen',
+        color: 'black'
+      }
+    };
+
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+            click={() => this.deletePersonHandler(index)}
+            name={person.name} 
+            age ={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, person.id)}/>
+          })}
+        
+      </div> 
+
+      );
+    
+    // over riding the style set before this. this is called scope styles
+    // when the persons tabs show, the toogle people person button changes red versus the standard green. Dynamic styling
+     style.backgroundColor ='red';
+    //  had to put square brackets cuz hover was a string in style
+     style[':hover'] ={
+       backgroundColor: 'salmon',
+       color: 'black'
+
+     }
+
+        }
+        // css class list
+        // will change this is working text based on criteria below
+        let classes = [];
+        if(this.state.persons.length <= 2) {
+          classes.push('red'); // classes = ['red'] 
+        }
+        if(this.state.persons.length <= 1) {
+          classes.push('bold'); // classes = ['red', 'bold']
+        }
+
+
+
     return (
-      <div className="App">
+      <StyleRoot>
+    <div className="App">
        <h1>Hi, I'm a React App</h1>
-       <p> This is really working!</p>
-       <button onClick={() =>this.switchNameHandler('Maxmillian!!')}>Switch Name </button>
-       <Person 
-       name={this.state.persons[0].name} 
-       age={this.state.persons[0].age}/>
-       <Person 
-       name={this.state.persons[1].name} 
-       age={this.state.persons[1].age}
-       click={this.switchNameHandler.bind(this, 'Max!')}
-       changed={this.nameChangedHandler}>My Hobbies:Racing</Person>
-       <Person 
-       name={this.state.persons[2].name} 
-       age={this.state.persons[2].age}/>
-      
-      </div>
+       {/* .join is used cuz classes is set to an empty array before being used here and css class list cant work without having something in the array */}
+       <p className={classes.join(' ')}> This is really working!</p>
+       <button
+        style={style} 
+        onClick={this.togglePersonsHandler}>Toggle Persons </button>
+       {persons}
+    </div>
+    </StyleRoot>
     );
   }
 }
 
-export default App;
+
+// higher order component, injecting more functionality into the app.js by wrapping it in Radium. Radium is about styling, allows css sudo selectors and medio queiries
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import Radium from 'radium';
 import './Person.css'
 
 
 
 const person = (props) => {
+    const style ={
+        // acceptable cuz its a string, understood cuz of radium
+        '@media (min-width: 500px)': {
+            // fixed width
+            width: '450px'
+        }
+    };
+
    return ( 
-        <div className="Person">
+        <div className="Person" style={style}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
@@ -17,4 +25,4 @@ const person = (props) => {
 };
 
 
-export default person;
+export default Radium(person);
